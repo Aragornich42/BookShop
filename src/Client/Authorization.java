706ea05fa5ee4e7e02aabb40cff0d7bdff7add5c
Vasiliy Kazhentsev <iitikbo0316kazhentsev@gmail.com>
2018-12-13 21:59:09 +0300

    SAD

--- src/Client/Authorization.java
+++ src/Client/Authorization.java
@@ -1,6 +1,10 @@
 package Client;
 
+import javafx.stage.Stage;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Authorization {
     private JTextField authField;
@@ -8,4 +12,35 @@ public class Authorization {
     private JButton registButton;
     private JLabel labelAuth;
     private JLabel labelReg;
+
+    public Authorization() {
+        authButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Operations oper = new Operations();
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+
+        registButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Registration reg = new Registration();
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public String getInfo() {
+        return this.authField.getText();
+    }
+
 }
--- src/Client/Registration.java
+++ src/Client/Registration.java
@@ -1,6 +1,8 @@
 package Client;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Registration {
     private JTextField F;
@@ -10,5 +12,24 @@ public class Registration {
     private JTextField Address;
     private JTextField Phone;
     private JTextField Email;
+
     private JButton зарегистрироватьсяButton;
+
+    public Registration() {
+        зарегистрироватьсяButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public String getInfo() {
+        return F.getText() + " " + I.getText() + " " + O.getText() + "|" + City.getText() + " "
+                + Address.getText() + ";" + Phone.getText() + ";" + Email.getText();
+    }
 }
